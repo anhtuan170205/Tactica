a8b3f35fd1368f07b264031873696f3b5f23b7de
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-03-10 16:40:32 +0700

    Grid System

---
+++ Assets/Scripts/Grid/GridPosition.cs
@@ -0,0 +1,42 @@
+using System;
+public struct GridPosition : IEquatable<GridPosition>
+{
+    public int x;
+    public int z;
+
+    public GridPosition(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+    public override string ToString()
+    {
+        return "x: " + x + " z: " + z;
+    }
+    public static bool operator ==(GridPosition a, GridPosition b)
+    {
+        return a.x == b.x && a.z == b.z;
+    }
+    public static bool operator !=(GridPosition a, GridPosition b)
+    {
+        return !(a == b);
+    }
+
+    public bool Equals(GridPosition other)
+    {
+        return x == other.x && z == other.z;
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj is GridPosition)
+        {
+            GridPosition gridPosition = (GridPosition)obj;
+            return x == gridPosition.x && z == gridPosition.z;
+        }
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, z);
+    }
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,11 +8,19 @@ public class Unit : MonoBehaviour
     [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
     }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
+    }
+
     private void Update()
     {
         float stopDistance = 0.1f;
@@ -26,6 +34,12 @@ public class Unit : MonoBehaviour
         else
         {
             unitAnimator.SetBool("isRunning", false);
+        }
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
         }
     }
     public void Move(Vector3 targetPosition)
